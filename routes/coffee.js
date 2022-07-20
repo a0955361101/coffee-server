3404f41cf3e7b6559937a677da60ddb1a88de08b
min <a0955361101@gmail.com>
2022-07-21 00:10:04 +0800

    外鍵SQL

--- routes/coffee.js
+++ routes/coffee.js
@@ -30,11 +30,26 @@ router.get('/', async (req, res) => {
         output.rows = r2;
     }
     const sql03 = "SELECT * FROM course";
+    // 外鍵SQL
+    // const sql03 = "SELECT * FROM`course` JOIN course_related ON `course`.`course_sid` = `course_related`.`course_sid`";
     const [r3] = await db.query(sql03);
     output = { ...output, page, totalRows, totalPages };
     // res.json(output);
+
+    // const sql = "INSERT INTO `course`(`course_name`, `course_price`, `course_level`, `course_img_s`, `course_content`, `course_people`, `course_material`) VALUES (?,?,?,?,?,?,?)";
+    // const { course_name, course_price, course_level, course_img_s, course_content, course_people, course_material } = req.body;
+    // const [result] = await db.query(sql, [course_name, course_price, course_level, course_img_s, course_content, course_people, course_material]);
+    // res.json(result);
 
     res.json(r3);
 });
 
 module.exports = router;
+
+
+
+// SELECT * FROM`主表單`
+// JOIN 要連結的表單
+// ON `主表單`.`主表的外鍵欄位` = `要連結的表單`.`要連結的表單的PK`
+// 外鍵SQL
+// SELECT * FROM`course` JOIN course_related ON `course`.`course_sid` = `course_related`.`course_sid`; 
